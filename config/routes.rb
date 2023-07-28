a1870f38d571296eb50dde00037978fac76001f6
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-28 13:57:51 -0400

    user model, user controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  resources :users, only: [:new, :create] # just create, no edit
+
+  get '/login', to: 'sessions#new', as: 'login' # user login form
+  post '/login', to: 'sessions#create' # logging user in
+  delete '/logout', to: 'sessions#destroy', as: 'logout' 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
