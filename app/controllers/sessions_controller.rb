efb94585e1d16e59a6c232aee399026605343aed
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-28 17:17:06 -0400

    finalize routes, minimal deign on forms, add login/logout and sign up button on nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,40 @@
 class SessionsController < ApplicationController
+  before_action :redirect_if_logged_in, only: [:new]
+  before_action :redirect_unless_logged_in, only: [:destroy]
+
+  def new
+    puts "HERE IN SESSIONS NEW"
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      puts "LOGIN SUCCESSFUL!"
+      redirect_to root_path
+    else
+      puts "WRONG PASSWORD"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    puts "LOGGED OUT!"
+    redirect_to root_path
+  end
+
+  private
+
+  def redirect_if_logged_in
+    if session[:user_id]
+      redirect_to root_path, alert: "You are already signed up and logged in!"
+    end
+  end
+
+  def redirect_unless_logged_in
+    unless session[:user_id]
+      redirect_to login_path, alert: "You must be logged in to do that!"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :redirect_if_logged_in, only: [:new]
+
   def new
     @user = User.new
   end
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       puts "Account created and stored"
-      redirect_to root_path, notice: "Account Created"
+      redirect_to root_path
     else
       flash.now[:alert] = @user.errors.full_messages.join(", ")
       render :new
@@ -20,6 +22,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def redirect_if_logged_in
+    if session[:user_id] # change this to your method for checking if a user is logged in
+      redirect_to root_path, notice: "You are already signed up and logged in!"
+    end
   end
 end
 
