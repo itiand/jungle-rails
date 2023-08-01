180245ebd4dd5f53b54387d99768b6f0207a3766
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-01 19:02:38 -0400

    home page test - products load

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,3 +1,8 @@
 it("There is products on the page", () => {
-  cy.get(".products article").should("be.visible");
+  cy.visit("/")
+  cy.get(".products").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.get(".products article").should("have.length", 2);
 });
