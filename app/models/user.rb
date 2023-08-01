2616b18d41f78999173f127eb74a41fa07d2b1d9
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-31 22:12:13 -0400

    testing for password and password confirmation, both must be present and must match. Add case_sensitive to false on User model. Also to lowercase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save :to_lowercase_email
+
+  private
+  def to_lowercase_email
+    self.email = self.email.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,50 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  #password and password_confirmation need to exist
+
   describe "Validation" do
-    category = Category.new(name: "Spiky Plants")
-    
+    #password and password_confirmation need to exist
+    it 'should not save if either password or password_confirmation is nil' do
+
+      user = User.new(email: 'c@yahoo.com',
+        password: '',
+        password_confirmation: ''
+      )
+
+      expect(user.save).to be false
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    #need to match
+    it 'should not save if password and password_confirmation do not match' do
+      user = User.new(
+        email: 'd@yahoo.com',
+        password: 'password',
+        password_confirmation: 'wrongPass'
+      )
+
+      expect(user.save).to be false
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
   end
-  #need to match
+
+  #case sensitive of email?
+  it 'validates uniqueness of email' do
+    User.create!(
+      email: 'test1@test.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+
+    user = User.new(
+    email: 'TEST1@TeST.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+
+    expect(user).to_not be_valid
+  end
 
   #email, first name, last name
 
