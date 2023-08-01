dc3e0d68dd246265e5ad5bdd1feb2cbaa8c74270
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-08-01 09:17:41 -0400

    model adjust - first_name and last_name necessary. Testing pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe User, type: :model do
     #password and password_confirmation need to exist
     it 'should not save if either password or password_confirmation is nil' do
 
-      user = User.new(email: 'c@yahoo.com',
+      user = User.new(
+        first_name: 'Chris',
+        last_name: 'Hawkins',
+        email: 'c@yahoo.com',
         password: '',
         password_confirmation: ''
       )
@@ -19,6 +22,8 @@ RSpec.describe User, type: :model do
     #need to match
     it 'should not save if password and password_confirmation do not match' do
       user = User.new(
+        first_name: 'Chris',
+        last_name: 'Hawkins',
         email: 'd@yahoo.com',
         password: 'password',
         password_confirmation: 'wrongPass'
@@ -33,21 +38,41 @@ RSpec.describe User, type: :model do
   #case sensitive of email?
   it 'validates uniqueness of email' do
     User.create!(
-      email: 'test1@test.com',
+      first_name: 'Chris',
+      last_name: 'Hawkins',
+      email: 'test4@test.com',
       password: 'password',
       password_confirmation: 'password'
     )
 
     user = User.new(
-    email: 'TEST1@TeST.com',
+    first_name: 'Chris',
+    last_name: 'Hawkins',
+    email: 'TEST4@TeST.com',
     password: 'password',
     password_confirmation: 'password'
     )
 
-    expect(user).to_not be_valid
+    expect(user.save).to be false
+    expect(user.errors.full_messages).to include("Email has already been taken")
   end
 
   #email, first name, last name
+  it 'should not save if either email, first name, last name are missing' do
+    user = User.new(
+      first_name: nil,
+      last_name: nil,
+      email: nil,
+      password: 'password',
+      password_confirmation: 'password'
+    )
 
+    expect(user.save).to be false
+    expect(user.errors.full_messages).to include(
+      "First name can't be blank",
+      "Last name can't be blank",
+      "Email can't be blank"
+      )
+  end
 
 end
