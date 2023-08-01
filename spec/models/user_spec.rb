b323201e7006d2c5e3df53ca2cd61ae50ba4baf6
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-31 21:01:38 -0400

    set up User model rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  #password and password_confirmation need to exist
+  describe "Validation" do
+    category = Category.new(name: "Spiky Plants")
+    
+  end
+  #need to match
+
+  #email, first name, last name
+
+
+end
